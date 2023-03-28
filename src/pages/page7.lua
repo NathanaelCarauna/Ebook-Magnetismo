4d102b32258509c1f9da8fda15c006e51d0b56c0
Nathanael carauna <nathanael.carauna@gmail.com>
2023-03-28 17:28:17 -0300

    Feat: rate compass depending on spark position, move spark when click button

--- src/pages/page7.lua
+++ src/pages/page7.lua
@@ -8,6 +8,7 @@ local ponteiro
 local bussola
 local device
 local switch_device
+local spark
 
 local function onBackPage(self, event)
     if event.phase == "ended" or event.phase == "cancelled" then
@@ -16,29 +17,48 @@ local function onBackPage(self, event)
         return true
     end
 end
+local function animateImage()
+    print("animateImage called")
+    transition.to(spark, { x = display.contentWidth * 0.22, y = display.contentHeight * 0.20, time = 1000 })
+    transition.to(spark, { x = display.contentWidth * 0.22, y = display.contentHeight * 0.33, time = 500, delay = 1000 })
+    transition.to(spark, { x = display.contentWidth * 0.77, y = display.contentHeight * 0.33, time = 1000, delay = 1500 })
+    transition.to(spark, { x = display.contentWidth * 0.77, y = display.contentHeight * 0.20, time = 500, delay = 2500 })    
+    print("animateImage finished")
+
+end
 
 local function updateCompass(event)
-    local rotation = display.getCurrentStage().contentWidth / display.getCurrentStage().contentHeight
-    rotation = 360 - math.deg(math.atan2(event.yGravity, event.xGravity)) + 90 - rotation
-    ponteiro.rotation = rotation
+    if switch_device.isOn == false and event.xGravity ~= nil then        
+        local rotation = display.getCurrentStage().contentWidth / display.getCurrentStage().contentHeight
+        rotation = 360 - math.deg(math.atan2(event.yGravity, event.xGravity)) + 90 - rotation
+        ponteiro.rotation = rotation
+    elseif switch_device.isOn then        
+        local diffX = spark.x - ponteiro.x
+        local diffY = spark.y - ponteiro.y
+
+        local angle = math.atan2(diffY, diffX) * 180 / math.pi
+        ponteiro.rotation = angle - 90
+    end
 end
 
 local function turnOnOff(event)
-
     if event.phase == "ended" then
-        if switch_device.isOn then
-            print("Device is turned off")
+        if switch_device.isOn then            
             switch_device.isOn = false
-            switch_device.fill.effect = "filter.grayscale"            
-            spark.isVisible = false
+            switch_device.fill.effect = "filter.grayscale"
+            spark.isVisible = false            
         else            
-            print("Device is turned On")
             switch_device.fill.effect = "filter.bloom"
             switch_device.isOn = true
-            spark.isVisible = true
-        end        
+
+            if (spark.isVisible == false) then
+                spark.x = display.contentWidth * 0.77
+                spark.y = display.contentHeight * 0.20
+                spark.isVisible = true                
+                animateImage()                
+            end
+        end
     end
-    
 end
 
 local function onNextPage(self, event)
@@ -62,40 +82,40 @@ function scene:create(event)
     sceneGroup:insert(background)
 
     local instructionsText = display.newImage('src/assets/texts/page7Instructions.png', display.actualContentWidth,
-    display.actualContentHeight)
+        display.actualContentHeight)
     instructionsText.x = display.contentWidth * 0.5
     instructionsText.y = display.contentHeight * 0.02
     sceneGroup:insert(instructionsText)
 
     local text = display.newImage('src/assets/texts/page7Text.png', display.actualContentWidth,
-    display.actualContentHeight)
-    text.x = display.contentWidth * 5/10
+        display.actualContentHeight)
+    text.x = display.contentWidth * 5 / 10
     text.y = display.contentHeight * 0.74
     sceneGroup:insert(text)
 
     device = display.newImage('src/assets/images/device.png', display.actualContentWidth,
-    display.actualContentHeight)
-    device.x = display.contentWidth * 5/10 +5
-    device.y = display.contentHeight * 6/20
+        display.actualContentHeight)
+    device.x = display.contentWidth * 5 / 10 + 5
+    device.y = display.contentHeight * 6 / 20
     device:scale(1, 1)
     sceneGroup:insert(device)
 
     bussola = display.newImage('src/assets/images/bussola.png', display.actualContentWidth,
-    display.actualContentHeight)
-    bussola.x = display.contentWidth * 5/10
+        display.actualContentHeight)
+    bussola.x = display.contentWidth * 5 / 10
     bussola.y = display.contentHeight * 0.263
     bussola:scale(0.05, 0.05)
     sceneGroup:insert(bussola)
 
     ponteiro = display.newImage('src/assets/images/bussola_ponteiro.png', display.actualContentWidth,
-    display.actualContentHeight)
-    ponteiro.x = display.contentWidth * 5/10
+        display.actualContentHeight)
+    ponteiro.x = display.contentWidth * 5 / 10
     ponteiro.y = display.contentHeight * 0.263
     ponteiro:scale(0.05, 0.05)
     sceneGroup:insert(ponteiro)
 
     spark = display.newImage('src/assets/images/spark.png', display.actualContentWidth,
-    display.actualContentHeight)
+        display.actualContentHeight)
     spark.x = display.contentWidth * 0.77
     spark.y = display.contentHeight * 0.20
     spark:scale(0.1, 0.1)
@@ -103,9 +123,9 @@ function scene:create(event)
     sceneGroup:insert(spark)
 
     switch_device = display.newImage('src/assets/images/switch_device.png', display.actualContentWidth,
-    display.actualContentHeight)    
-    switch_device.x = display.contentWidth * 7/10
-    switch_device.y = display.contentHeight * 5/10
+        display.actualContentHeight)
+    switch_device.x = display.contentWidth * 7 / 10
+    switch_device.y = display.contentHeight * 5 / 10
     switch_device.fill.effect = "filter.grayscale"
     switch_device:scale(0.1, 0.1)
     sceneGroup:insert(switch_device)
@@ -136,7 +156,8 @@ function scene:show(event)
 
         forwardButton.touch = onNextPage
         forwardButton:addEventListener("touch", forwardButton)
-        Runtime:addEventListener("accelerometer", updateCompass)        
+        Runtime:addEventListener("accelerometer", updateCompass)
+        Runtime:addEventListener("enterFrame", updateCompass)
         switch_device:addEventListener("touch", turnOnOff)
         switch_device.fill.effect = "filter.grayscale"
         switch_device.isOn = false
@@ -152,6 +173,7 @@ function scene:hide(event)
         forwardButton:removeEventListener("touch", forwardButton)
         background:removeEventListener("tap", background)
         Runtime:removeEventListener("accelerometer", updateCompass)
+        Runtime:removeEventListener("enterFrame", updateCompass)
         switch_device:removeEventListener("touch", turnOnOff)
         spark.isVisible = false
     elseif (phase == "did") then
