044baec9f0274655d59699479c89392aacca9e1f
Nathanael carauna <nathanael.carauna@gmail.com>
2023-03-28 20:50:34 -0300

    Fix: fix return to page bug

--- src/pages/page8.lua
+++ src/pages/page8.lua
@@ -1,8 +1,9 @@
 local composer = require("composer")
 local scene = composer.newScene()
 local physics = require("physics")
-physics.start()
-physics.setGravity(0, 0)
+-- physics.start()
+-- physics.setGravity(0, 0)
+
 
 local DISTANCIA_RELPUSAO = 150
 
@@ -12,6 +13,8 @@ local background
 local raio_solar
 local earth
 local sun
+local limit_left, limit_right, limit_Up, limit_bottom
+
 
 -- display.setDrawMode("debug")
 
@@ -83,57 +86,50 @@ function scene:create(event)
     sun.x = display.contentWidth * 10 / 10
     sun.y = display.contentHeight * 0.6
     sun:scale(0.5, 0.5)
-    physics.addBody(sun, "static", { density = 1.6, friction = 0.5, radius= 40})
     sceneGroup:insert(sun)
-
+    
     earth = display.newImage('src/assets/images/earth.png', display.actualContentWidth,
-        display.actualContentHeight)
+    display.actualContentHeight)
     earth.x = display.contentWidth * 2 / 10
     earth.y = display.contentHeight * 0.7
     earth:scale(0.1, 0.1)
-    physics.addBody(earth, "static", { density = 1.6, friction = 0.5, radius= 30})
     sceneGroup:insert(earth)
-
+    
     raio_solar = display.newImage('src/assets/images/raio_solar.png', display.actualContentWidth,
-        display.actualContentHeight)
+    display.actualContentHeight)
     raio_solar.x = display.contentWidth * 9 / 10
     raio_solar.y = display.contentHeight * 0.6
     raio_solar:scale(0.1, 0.1)
-    physics.addBody(raio_solar, "dinamic", { density = 1.6, radius = 20, bounce=0.7})
     sceneGroup:insert(raio_solar)
-
-    local limit_left = display.newRect(-40, 0, 40, display.contentHeight)
+    
+    limit_left = display.newRect(-40, 0, 40, display.contentHeight)
     limit_left.anchorX = 0
     limit_left.anchorY = 0
-    physics.addBody(limit_left, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
     sceneGroup:insert(limit_left)
-
-    local limit_right = display.newRect(display.contentWidth, 0, 40, display.contentHeight)
+    
+    limit_right = display.newRect(display.contentWidth, 0, 40, display.contentHeight)
     limit_right.anchorX = 0
     limit_right.anchorY = 0
-    physics.addBody(limit_right, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
     sceneGroup:insert(limit_right)
-
-    local limit_bottom = display.newRect(0, display.contentHeight, display.contentWidth, 40)
+    
+    limit_bottom = display.newRect(0, display.contentHeight, display.contentWidth, 40)
     limit_bottom.anchorX = 0
     limit_bottom.anchorY = 0
-    physics.addBody(limit_bottom, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
     sceneGroup:insert(limit_bottom)
-
-    local limit_Up = display.newRect(0, -40, display.contentWidth, 40)
+    
+    limit_Up = display.newRect(0, -40, display.contentWidth, 40)
     limit_Up.anchorX = 0
     limit_Up.anchorY = 0
-    physics.addBody(limit_Up, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
     sceneGroup:insert(limit_Up)
-
+    
     backButton = display.newImage('src/assets/buttons/lightButtonLeft.png', display.contentWidth,
-        display.contentWidth)
+    display.contentWidth)
     backButton.x = display.contentWidth * 0.1
     backButton.y = display.contentHeight * 0.9
     sceneGroup:insert(backButton)
-
+    
     forwardButton = display.newImage('src/assets/buttons/lightButtonRight.png', display.contentWidth,
-        display.contentWidth)
+    display.contentWidth)
     forwardButton.x = display.contentWidth * 0.9
     forwardButton.y = display.contentHeight * 0.9
     sceneGroup:insert(forwardButton)
@@ -144,13 +140,20 @@ function scene:show(event)
     local phase = event.phase
 
     if (phase == "will") then
-
+        
     elseif (phase == "did") then
         backButton.touch = onBackPage
         backButton:addEventListener("touch", backButton)
-
+        
         physics.start()
         physics.setGravity(0, 0)
+        physics.addBody(earth, "static", { density = 1.6, friction = 0.5, radius= 30})
+        physics.addBody(sun, "static", { density = 1.6, friction = 0.5, radius= 40})
+        physics.addBody(raio_solar, "dinamic", { density = 1.6, radius = 20, bounce=0.7})
+        physics.addBody(limit_left, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
+        physics.addBody(limit_right, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
+        physics.addBody(limit_bottom, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
+        physics.addBody(limit_Up, "static", { density = 1.6, friction = 0.5, bounce = 0.2 })
 
         forwardButton.touch = onNextPage
         forwardButton:addEventListener("touch", forwardButton)
@@ -167,11 +170,11 @@ function scene:hide(event)
         raio_solar.x = display.contentWidth * 9 / 10
         raio_solar.y = display.contentHeight * 0.6
 
+        Runtime:removeEventListener("enterFrame", repelirMeteoro)
         backButton:removeEventListener("touch", backButton)
         forwardButton:removeEventListener("touch", forwardButton)
         background:removeEventListener("tap", background)
         Runtime:removeEventListener("accelerometer", moveMeteor)
-        Runtime:removeEventListener("enterFrame", repelirMeteoro)
         physics.stop()
     elseif (phase == "did") then
 
